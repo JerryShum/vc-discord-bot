37ae7e1771f15078295935a456f73cd3f690433b
Jerry <jerryshum2636@gmail.com>
2024-11-22 22:35:04 -0500

    remove owner and general user

--- botTTS.py
+++ botTTS.py
@@ -11,8 +11,8 @@ TEXT_CHANNEL_ID = int(os.getenv("TEXT_CHANNEL_ID"))
 VOICE_CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID"))
 VELARIE_USER_ID = int(os.getenv("VELARIE_USER_ID"))
 ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID"))
-OWNER_USER_ID = int(os.getenv("OWNER_USER_ID"))
-GENERAL_USER_ID = int(os.getenv("GENERAL_USER_ID"))
+# OWNER_USER_ID = int(os.getenv("OWNER_USER_ID"))
+# GENERAL_USER_ID = int(os.getenv("GENERAL_USER_ID"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -37,7 +37,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.channel.id == TEXT_CHANNEL_ID and (message.author.id == VELARIE_USER_ID or message.author.id == ADMIN_USER_ID or message.author.id == OWNER_USER_ID or message.author.id == GENERAL_USER_ID):
+    if message.channel.id == TEXT_CHANNEL_ID and (message.author.id == VELARIE_USER_ID or message.author.id == ADMIN_USER_ID):
         if voice_connection is None:
             await connect_to_voice_channel()
         
